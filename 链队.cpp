c7582b37a0ac78b25729139e4cdd39ffe6c522d4
tom <tom>
2018-09-04 09:15:29 +0800

    理解了一下链队，添加注释

--- 链队.cpp
+++ 链队.cpp
@@ -22,7 +22,7 @@ typedef struct LinkQue {
 } *LinkQuePtr;
 status initQue(LinkQuePtr que)
 {
-	que->front = que->rear = (Node*)malloc(sizeof(Node));
+	que->front = que->rear = (Node*)malloc(sizeof(Node));//头结点
 	if (!que->front)
 		return ERROR;//动态内存分配失败
 	que->count = 0;
@@ -34,24 +34,24 @@ status enQue(LinkQuePtr que, ElemType e)
 	Node * node = (Node *)malloc(sizeof(Node));
 	if (!node)
 	{
-		printf("内存分配失败");
-		getchar();
-		system("pause");
+		printf("内存分配失败\n");
 		return ERROR;
 	}
 	node->data = e;
 	node->next = NULL;
-	que->rear->next = node;
-	que->rear = node;
+	que->rear->next = node; //que->rear指向原队列的对尾，	que->rear->next = node; 将
+							//原队列尾的那个元素的指针指向新元素，那新元素就是新的对尾了。
+	que->rear = node;		//将rear指向新队列的对尾
 	return OK;
 }
 status deQue(LinkQuePtr que, ElemType * e)
 {
+	//que->front指向的是头节点，que->front->next指向的是队列的头
 	if (que->front->next != NULL)
 	{
 		Node * node = que->front->next;
-		*e = node->data;
-		que->front->next = que->front->next->next;
+		*e = node->data;//值拷贝不是地址拷贝，所以下面free（）不会对这个值有影响
+		que->front->next = que->front->next->next;//front指向删除了原来队列头后的新的队列头
 		free(node);
 		return OK;
 	}
@@ -61,7 +61,7 @@ int main()
 {
 	LinkQue que;
 	initQue(&que);
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10000000; i++)
 	{
 		printf("%d 尝试入队\n", i);
 		if (!enQue(&que, i))
